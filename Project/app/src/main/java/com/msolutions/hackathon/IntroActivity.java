30c599074fa81addad1e1e3f252af8115df24e4f
Kipps014 <zkipping014@rsdmo.org>
2014-11-08 22:56:41 -0600

    MADE THE THING WINK
    YAY

--- Project/app/src/main/java/com/msolutions/hackathon/IntroActivity.java
+++ Project/app/src/main/java/com/msolutions/hackathon/IntroActivity.java
@@ -4,6 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class IntroActivity extends ActionBarActivity {
@@ -14,6 +20,35 @@ public class IntroActivity extends ActionBarActivity {
         setContentView(R.layout.activity_intro);
     }
 
+    public void Wink(final View v) {
+        final Random rand = new Random();
+        final int random = rand.nextInt(3);
+        final ImageView Left = (ImageView) findViewById(R.id.LeftWink);
+        final ImageView Right = (ImageView) findViewById(R.id.RightWink);
+        v.setVisibility(View.INVISIBLE);
+        if (random == 1) {
+            Left.setVisibility(View.VISIBLE);
+            Right.setVisibility(View.INVISIBLE);
+        }
+        else if (random == 2) {
+            Right.setVisibility(View.VISIBLE);
+            Left.setVisibility(View.INVISIBLE);
+        }
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread( new Runnable() {
+                    @Override
+                    public void run() {
+                        v.setVisibility(View.VISIBLE);
+                        Right.setVisibility(View.INVISIBLE);
+                        Left.setVisibility(View.INVISIBLE);
+                    }
+                });
+            }
+        }, 100);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
